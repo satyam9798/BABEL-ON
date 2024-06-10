132614e523ba657b84a209f04baa65bafa7bb66f
satyam9798 <satyamshivam9798@gmail.com>
2024-06-11 00:33:38 +0530

    handle deep linking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import logo from './logo.jpg';
-import './App.css';
+import logo from "./logo.jpg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/app/chat/:userType/:roomId/:chatType/:linkType"
+          element={<HandleDeepLink />}
+        />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   );
-}
+};
+const HandleDeepLink = () => {
+  const history = useNavigate();
+  const { userType, roomId, chatType, linkType } = useParams();
+  console.log("path segment", userType, roomId, chatType, linkType);
+
+  useEffect(() => {
+    // const apiUrl = `www.heroku123.com/redirect1/${pathSegments}`;
+    const pathSegments = `${userType}/${roomId}/${chatType}/${linkType}`;
+    const apiUrl = `https://bableon-django-1193e2d277c3.herokuapp.com/chat/redirect1`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        // const redirectUrl = response.data.redirectUrl;
+        // window.location.href = redirectUrl;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [userType, roomId, chatType, linkType]);
+
+  return <div>Redirecting...</div>;
+};
 
 export default App;
